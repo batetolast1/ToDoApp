0b268a84c2b0180a1499dd335a0fe7ab6bc877a4
batetolast1 <batetolast1@gmail.com>
2020-10-25 11:53:43 +0100

    added feature: displaying modal alert when countdown is finished

--- js/services/renderService.js
+++ js/services/renderService.js
@@ -3,10 +3,17 @@ class RenderService {
     constructor() {
         this.taskAddingForm = document.querySelector(".js-task-adding-form");
         this.main = document.querySelector("main");
+
+        this.modal = document.querySelector('.modal');
+        this.modalBackdrop = document.querySelector(".modal-backdrop");
+        this.modalTitle = document.querySelector(".modal-title");
+        this.modalDescription = document.querySelector(".modal-description");
+
         this.apiService = new ApiService();
 
         this.addEventToTaskAddingForm();
         this.renderAllTasks();
+        this.addEventsToCloseModal();
     }
 
     addEventToTaskAddingForm() {
@@ -279,6 +286,10 @@ class RenderService {
 
                         this.apiService.updateOperation(operation)
                             .then(() => {
+                                this.openModal();
+                                this.modalTitle.innerText = "Time is up!";
+                                this.modalDescription.innerText = "Operation: \"" + operation.description + "\" is finished!";
+
                                 countdownDiv.parentElement.removeChild(countdownDiv);
                                 controlDiv.classList.remove("d-none");
                                 li.querySelectorAll(".js-operation-open-only")
@@ -347,5 +358,30 @@ class RenderService {
         const minutes = Math.floor((timeSpent / 60) % 60);
         const seconds = timeSpent % 60;
         return hours + "h " + minutes + "m " + seconds + "s";
+    }
+
+    addEventsToCloseModal() {
+        // close modal when clicking anywhere outside modal
+        window.addEventListener("click", event => {
+            if (event.target === this.modal) {
+                this.closeModal();
+            }
+        });
+
+        // close modal when clicking on x and 'close' button
+        document.querySelectorAll(".modal-close")
+            .forEach(element => element.addEventListener("click", () => this.closeModal()));
+    }
+
+    closeModal() {
+        this.modalBackdrop.style.display = "none";
+        this.modal.style.display = "none";
+        this.modal.classList.remove("show");
+    }
+
+    openModal() {
+        this.modalBackdrop.style.display = "block";
+        this.modal.style.display = "block";
+        this.modal.classList.add("show");
     }
 }
